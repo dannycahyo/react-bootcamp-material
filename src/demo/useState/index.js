7a2e092670462bb87c7522aa6e89351daad63324
dannycahyo <dannydwicahyono@gmail.com>
2023-06-02 21:24:10 +0700

    feat: add the useState demo code

---
+++ src/demo/useState/index.js
@@ -0,0 +1,21 @@
+const rootElement = document.getElementById("root");
+
+const App = () => {
+  const [counter, setCounter] = React.useState(0);
+
+  return (
+    <div>
+      <button
+        onClick={() => {
+          setCounter(counter + 1);
+          alert(`counter ${counter}`);
+        }}
+      >
+        Counter
+      </button>
+      <p>{counter}</p>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, rootElement);
