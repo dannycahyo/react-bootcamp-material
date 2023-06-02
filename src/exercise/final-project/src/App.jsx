266d3051fed66ef412ab8710fa81fcc9777451e1
dannycahyo <dannydwicahyono@gmail.com>
2023-06-02 21:47:15 +0700

    feat: add the final project exercise initial set up

---
+++ src/exercise/final-project/src/App.jsx
@@ -0,0 +1,17 @@
+// You can use the Product Card & The Styling If You Want To
+// import ProductCard from './components/ProductCard'; 
+// import "./style/ProductCard.css"
+
+import './App.css'
+
+
+const ProductPage = () => {
+  
+  return (
+    <div>
+      <h1>Product List</h1>
+    </div>
+  );
+};
+
+export default ProductPage;
