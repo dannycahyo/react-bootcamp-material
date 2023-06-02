b3c233e04dc1f5399e789019d130592f0b881ffc
dannycahyo <dannydwicahyono@gmail.com>
2023-06-02 21:42:25 +0700

    feat: add the exercise initial set up

---
+++ src/exercise/basic-product-page/react-api/index.js
@@ -0,0 +1,5 @@
+const rootElement = document.getElementById("root");
+
+/*  Your Work Over Here  */
+
+// ReactDOM.render([put the element here], rootElement);
---
+++ src/exercise/basic-product-page/vanilla-js/index.js
@@ -0,0 +1,5 @@
+const rootElement = document.getElementById("root");
+
+/*  Your Work Over Here  */
+
+// rootElement.append(put the element here);
